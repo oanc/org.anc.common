1d2cbad1122efa5716b6cd84961e7a7ee4d767d5
ksuderman <ksuderman@edd201f6-e76c-4490-bd89-b6241560e6fd>
2010-03-18 19:18:53 +0000

    Added constant definitions for the MASC annotation set types.

---
+++ common/trunk/src/main/java/org/anc/masc/MASC.java
@@ -0,0 +1,28 @@
+package org.anc.masc;
+
+public final class MASC
+{
+   public final class AnnotationSets
+   {
+      public final class XCES
+      {
+         public static final String NAME = "xces";
+         public static final String TYPE = "http://www.xces.org/schema/2003";
+      }
+      public final class PTB
+      {
+         public static final String NAME = "PTB";
+         public static final String TYPE = "http://www.cis.upenn.edu/~treebank/";
+      }
+      public final class FrameNet
+      {
+         public static final String NAME = "FrameNet";
+         public static final String TYPE = "http://framenet.icsi.berkeley.edu/";
+      }
+      public final class WordNet
+      {
+         public static final String NAME = "WordNet";
+         public static final String TYPE = "http://wordnet.princeton.edu/";
+      }
+   }
+}
