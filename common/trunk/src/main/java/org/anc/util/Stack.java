9c440e585ce2015d7b5f372e6118431510ede0f9
ksuderman <ksuderman@edd201f6-e76c-4490-bd89-b6241560e6fd>
2011-09-24 18:21:04 +0000

    Added the StopWatch, Stack, and Properties classes to org.anc.util.

--- common/trunk/src/main/java/org/anc/util/Stack.java
+++ common/trunk/src/main/java/org/anc/util/Stack.java
@@ -1,3 +1,19 @@
+/*-
+ * Copyright 2011 The American National Corpus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package org.anc.util;
 
 import java.util.ArrayList;
@@ -7,7 +23,7 @@ import java.util.ArrayList;
  * stack is implemented with an {@link ArrayList} with the
  * "top" of the stack at the end of the list.
  * <p>
- * This implementation only includes the basic stack operations and does
+ * This implementation only includes the basic stack operations and does not
  * extend any Java collection class or implement any interface. None of 
  * the methods throw exceptions and <code>peek</code> and <code>pop</code>
  * return <code>null</code> if the stack is empty when they are called.
